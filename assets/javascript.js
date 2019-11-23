f19421e16422903c31a61721fa6181ecac51201f
Kevin Suh <kevinsuh@Kevins-MacBook-Pro.local>
2019-11-23 10:18:03 -0600

    Event div colors are set, all done. Except the page is ugly LOL

--- assets/javascript.js
+++ assets/javascript.js
@@ -1,6 +1,8 @@
 var timeSlotsArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM", "5PM"]
 var eventsArray = ["", "", "", "", "", "", "", "", ""]
 var scheduleEl = $("#schedule")
+var time = moment().format("LT")
+var currentHour = time[0] + time[1] + time[6] +time[7]
 
 if (localStorage.getItem("eventsArray")) {
     eventsArray = JSON.parse(localStorage.getItem("eventsArray"))
@@ -18,6 +20,18 @@ function renderSchedule() {
         var eventDiv = $("<textarea>").addClass("event").attr("id", timeSlotsArray[i] + "event")
         eventDiv.text(eventsArray[i])
         newRow.append(eventDiv)
+
+        //Color the different event divs
+
+        if (timeSlotsArray[i] === currentHour) {
+            eventDiv.css("background-color", "red")
+        }
+        else if (timeSlotsArray.indexOf(currentHour) > i) {
+            eventDiv.css("background-color", "gray")
+        }
+        else {
+            eventDiv.css("background-color", "green")
+        }
 
         var saveButton = $("<button>").addClass("save").attr("id", timeSlotsArray[i] + "save")
         saveButton.text("Save")

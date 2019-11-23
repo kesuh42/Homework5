9396cf7f3a245bf3d840f16b5a8359f810f89635
Kevin Suh <kevinsuh@Kevins-MacBook-Pro.local>
2019-11-23 11:06:29 -0600

    Added some comments

--- assets/javascript.js
+++ assets/javascript.js
@@ -2,17 +2,22 @@ var timeSlotsArray = ["9AM", "10AM", "11AM", "12PM", "1PM", "2PM", "3PM", "4PM",
 var eventsArray = ["", "", "", "", "", "", "", "", ""]
 var scheduleEl = $("#schedule")
 var time = moment().format("LT")
+//currentHour will evaluate to hour and AM/PM, ex. "10PM"
 var currentHour = time[0] + time[1] + time[6] +time[7]
 
+//Pull the events from local storage if present
 if (localStorage.getItem("eventsArray")) {
     eventsArray = JSON.parse(localStorage.getItem("eventsArray"))
 }
 
-
+//Render the schedule
 function renderSchedule() {
+    //Making a schedule row for every element of timeSlotsArray
     for (var i = 0; i< 9; i++) {
         var newRow = $("<div>").addClass("schedulerow").attr("id", timeSlotsArray[i])
 
+
+        //Adding column for the time, the event, and a save button (three divs)
         var timeDiv = $("<div>").addClass("time")
         timeDiv.text(timeSlotsArray[i])
         newRow.append(timeDiv)
@@ -22,7 +27,6 @@ function renderSchedule() {
         newRow.append(eventDiv)
 
         //Color the different event divs
-
         if (timeSlotsArray[i] === currentHour) {
             eventDiv.css("background-color", "red")
         }
@@ -42,8 +46,10 @@ function renderSchedule() {
     }
 }
 
+//Initial page render
 renderSchedule()
 
+//Set up the save button function and event listener
 function updateEvents() {
     if (event.target.getAttribute("class") === "save") {
         for (var i = 0; i< 9; i++) {
